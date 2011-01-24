4964b6875e56f1d8c7a925bbc46ee338fe1e826c
Linus Oleander <linus@oleander.nu>
2011-01-24 13:56:43 +0100

    Updating the gemspec file

--- spec/movie_searcher_spec.rb
+++ spec/movie_searcher_spec.rb
@@ -1,1 +1,7 @@
 require 'spec_helper'
+
+describe MovieSearcher do
+  it "should return nil if the movie title is to long" do
+    MovieSearcher.find("asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd").should be_nil
+  end
+end
