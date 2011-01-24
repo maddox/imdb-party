9369d5a5a93e7416dde5f958e91d02df80926605
Linus Oleander <linus@oleander.nu>
2011-01-24 21:47:27 +0100

    Implementing some old test in rspec

--- spec/movie_searcher_spec.rb
+++ spec/movie_searcher_spec.rb
@@ -42,6 +42,80 @@ describe MovieSearcher do
   it "should return nil if nil is being passed to it" do
     MovieSearcher.find(nil).should be_nil
   end
-  
+end
 
+describe MovieSearcher, "should work as before" do
+  before(:all) do
+    @movie = MovieSearcher.find_movie_by_id("tt0382932")
+  end
+
+  it "have a title" do
+    @movie.title.should eq("Ratatouille")
+  end
+
+  it "have an imdb_id" do
+    @movie.imdb_id.should eq("tt0382932")
+  end
+
+  it "have a tagline" do
+    @movie.tagline.should eq("Dinner is served... Summer 2007")
+  end
+
+  it "have a plot" do
+    @movie.plot.should eq("Remy is a young rat in the French countryside who arrives in Paris, only to find out that his cooking idol is dead. When he makes an unusual alliance with a restaurant's new garbage boy, the culinary and personal adventures begin despite Remy's family's skepticism and the rat-hating world of humans.")
+  end
+
+  it "have a runtime" do
+    @movie.runtime.should eq("111 min")
+  end
+
+  it "have a rating" do
+    @movie.rating.should eq(8.1)
+  end
+
+  it "have a poster_url" do
+    @movie.poster_url.should match(/http:\/\/ia.media-imdb.com\/images\/.*/)
+  end
+
+  it "have a release date" do
+    @movie.release_date.should eq(DateTime.parse("2007-06-29"))
+  end
+
+  it "have a certification" do
+    @movie.certification.should  eq("G")
+  end
+
+  it "have trailers" do
+    @movie.trailers.should be_instance_of(Hash)
+    @movie.should have(4).trailers
+    @movie.trailers[@movie.trailers.keys.first].should eq("http://www.totaleclips.com/Player/Bounce.aspx?eclipid=e27826&bitrateid=461&vendorid=102&type=.mp4")
+  end
+
+  it "have genres" do
+    @movie.genres.should be_instance_of(Array)
+    @movie.should have(4).genres
+  end
+
+  it "have actors" do
+    @movie.actors.should be_instance_of(Array)
+    @movie.should have(4).actors
+  end
+
+  it "have directors" do
+    @movie.directors.should be_instance_of(Array)
+    @movie.should have(2).directors
+  end
+
+  it "have writers" do
+    @movie.writers.should be_instance_of(Array)
+    @movie.should have(2).writers
+  end
+  
+  it "always return a date object when requesting the release date" do
+    [{:imdb => "tt0066026", :date => "1970-03-01"}, {:imdb => "tt1446072", :date => "2010-11-27"}].each do |value|
+      movie = MovieSearcher.find_movie_by_id(value[:imdb])
+      movie.release_date.strftime('%Y %m %d').should eq(DateTime.parse(value[:date]).strftime('%Y %m %d'))
+      movie.release_date.should be_instance_of(Date)
+    end
+  end
 end
