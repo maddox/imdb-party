1c2315e20ec8fd9c4a7674b4d3cec970394b60e7
Linus Oleander <linus@oleander.nu>
2011-01-25 14:19:48 +0100

    Implementing my own version of Levenshtein based on the HintableLevenshtein gem

---
+++ spec/levenshtein_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Levenshtein do
+  it "should return the right value when no threshold is being defined" do
+    Levenshtein.distance("first", "last").should eq(0.6)
+  end
+  
+  it "should return nil value when a threshold is being passed" do
+    Levenshtein.distance("first", "last", 0.1).should be_nil
+  end
+  
+  it "should not return nil value when a threshold is being passed" do
+    Levenshtein.distance("first", "last", 0.8).should_not be_nil
+  end
+  
+  it "should not change when a threshold is being passed" do
+    Levenshtein.distance("first", "last", 0.8).should eq(0.6)
+  end
+  
+  it "should be tested?" do
+    Levenshtein.distance("first", "").should eq(1)
+  end
+  
+  it "should return zero when to strings are equal" do
+    Levenshtein.distance("abc123", "abc123").should eq(0)
+  end
+  
+  it "should never return a value larger then 1" do
+    Levenshtein.distance("asdasdasdasdasdasdasdasdasdasdasdasd", "a").should <= 1
+  end
+end
