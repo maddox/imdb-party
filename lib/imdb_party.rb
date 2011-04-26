9509cc82243fd2ac66f18f713753e4edb66ecdae
Jon Maddox <jon@mustacheinc.com>
2011-04-26 09:14:44 -0400

    garbage

--- lib/imdb_party.rb
+++ lib/imdb_party.rb
@@ -6,6 +6,3 @@ require File.join(directory, 'imdb_party', 'httparty_icebox')
 require File.join(directory, 'imdb_party', 'imdb')
 require File.join(directory, 'imdb_party', 'movie')
 require File.join(directory, 'imdb_party', 'person')
-
-
-puts ImdbParty::Imdb.new.find_movie_by_id('tt1391034').inspect
